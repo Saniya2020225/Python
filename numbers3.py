0cf96fb976b90c15e7224cd8280f91ef1fffc7f9
saniya kushwaha <saniyakushwaha59@gmail.com>
2026-04-03 01:21:37 +0530

    python

---
+++ numbers3.py
@@ -0,0 +1,22 @@
+import math
+print(round(3.2))#3
+print(round(3.7))#4
+print(round(3.5))#4
+
+print(abs(-3.8))#3.8
+print(abs(3.8))#3.8
+
+print(math.ceil(2.3))#3
+print(math.floor(2.3))#2
+print(math.trunc(2.3))#2
+
+print(math.fabs(-2.3))#2.3
+print(math.fabs(2.3))#2.3
+
+print(math.factorial(5))#5*4*3*2*1
+print(math.isqrt(36))#6
+print(math.gcd(2, 3))#1
+print(math.lcm(2, 3))#6
+print(math.perm(3, 1))#3 ncr
+print(math.fmod(10, 3))#10%3 = 1
+print(math.sqrt(36.5))#6
---
+++ typeconversion.py
@@ -0,0 +1,16 @@
+x = input("x:")
+print(type(x))
+
+y = int(x) + 1  # x+1
+print(f"x: {x}, y: {y}")
+
+print(bool(0))
+print(bool(1))
+print(bool(-1))
+print(bool(7))
+print(bool(""))
+print(bool("saniya"))
+print(bool("true"))
+
+# print 0 or null to false hoga
+# print kuch bhi likha ho space bhi ho to true hoga
